849ee98b932d9dfb8ba4cf164e655eadaf445af9
Alex Boada <92454432+Thor87Col@users.noreply.github.com>
2023-11-12 15:46:59 +0000

    Ajustes Polynomial

--- Scripts/Polynomial.py
+++ Scripts/Polynomial.py
@@ -6,112 +6,133 @@ Created on Fri Oct 20 15:22:20 2023
 @author: Equipo DSA
 """
 
-# Importe el conjunto de datos de diabetes y divídalo en entrenamiento y prueba usando scikit-learn
-from sklearn.model_selection import train_test_split
-from sklearn.datasets import load_diabetes
-
-db = load_diabetes()
-X = db.data
-y = db.target
-X_train, X_test, y_train, y_test = train_test_split(X, y)
-
 # Importar Librerias
-import numpy as np
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import PolynomialFeatures
-from sklearn.pipeline import make_pipeline
-from sklearn.metrics import mean_squared_error, mean_absolute_error
-from sklearn.preprocessing import LabelEncoder
-
-
-#Importe MLFlow para registrar los experimentos, el regresor de bosques aleatorios y la métrica de error cuadrático medio
-import mlflow
+import numpy as np
 import mlflow.sklearn
+import mlflow
+import geopandas as gpd
 import datetime
+from sklearn.preprocessing import robust_scale
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import LabelEncoder, PolynomialFeatures, robust_scale
+from sklearn.preprocessing import LabelEncoder
+from sklearn.pipeline import make_pipeline
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, mean_absolute_error
+from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import RandomForestRegressor
-# from sklearn.metrics import mean_squared_error
+from sklearn.cluster import KMeans
+from sklearn import metrics
 
 # defina el servidor para llevar el registro de modelos y artefactos
 #mlflow.set_tracking_uri('http://localhost:5000')
 
+evaluaciones = pd.read_csv('/workspaces/MIAD_DSA_rotacion_cultivos/data/Evaluaciones_Agropecuarias_Municipales_EVA.csv')
 
-# registre el experimento
-experiment = mlflow.set_experiment("Booster")
-#nombre incremental
-time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-#nombre incremental
+municipios = gpd.read_file('/workspaces/MIAD_DSA_rotacion_cultivos/data/MunicipiosVeredas19MB.json')
 
-run_name=f"Booster-{time}"
-# Aquí se ejecuta MLflow sin especificar un nombre o id del experimento. MLflow los crea un experimento para este cuaderno por defecto y guarda las características del experimento y las métricas definidas. 
-# Para ver el resultado de las corridas haga click en Experimentos en el menú izquierdo. 
-with mlflow.start_run(run_name=run_name):
-    # defina los parámetros del modelo
-    n_estimators = 200 
-    max_depth = 6
-    max_features = 4
-    # Cree el modelo con los parámetros definidos y entrénelo
-    rf = RandomForestRegressor(n_estimators = n_estimators, max_depth = max_depth, max_features = max_features)
-    rf.fit(X_train, y_train)
-    # Realice predicciones de prueba
-    predictions = rf.predict(X_test)
-  
-    # Registre los parámetros
-    mlflow.log_param("num_trees", n_estimators)
-    mlflow.log_param("maxdepth", max_depth)
-    mlflow.log_param("max_feat", max_features)
-  
-    # Registre el modelo
-    mlflow.sklearn.log_model(rf, "random-forest-model")
-  
-    # Cree y registre la métrica de interés
-    mse = mean_squared_error(y_test, predictions)
-    mlflow.log_metric("mse", mse)
-    print(mse)
+# Filtrar columnas de interés en el DataFrame de evaluaciones
+evaluaciones2 = evaluaciones[['CÓD. MUN.', 'CULTIVO']]
 
+# Crear una tabla pivot
+pivot = np.round(pd.pivot_table(evaluaciones2, index='CÓD. MUN.',
+                                columns='CULTIVO', aggfunc=len, fill_value=0))
+pivot.reset_index(inplace=True)
+pivot['DPTOMPIO'] = pivot['CÓD. MUN.']
 
+# Asumiendo que 'municipios' es un DataFrame existente
+municipios['DPTOMPIO'] = municipios[['DPTOMPIO']].apply(pd.to_numeric)
+municipios2 = municipios[['DPTOMPIO', 'geometry']]
+pivot = pivot.astype({'CÓD. MUN.': 'int'})
+municipios3 = pd.merge(municipios2, pivot, left_on='DPTOMPIO', right_on='DPTOMPIO')
+municipios3 = municipios3.iloc[:, 1:]
 
-with open('../data/Evaluaciones_Agropecuarias_Municipales_EVA.csv', 'r', encoding='utf-8') as file:
-    evaluaciones = pd.read_csv(file)
+# Calcular distancias pairwise
+distances = metrics.pairwise_distances(
+    municipios3.loc[:, ~municipios3.columns.isin(['geometry', 'CÓD. MUN.'])].head()
+).round(4)
 
-columnas = ['NOMBRE_CULTIVO', 'ANIO', 'NOMBRE_MUNICIPIO', 'RENDIMIENTO_TONELADAS_HA']
-df = evaluaciones[columnas]
+# Escalar los datos
+db_scaled = robust_scale(municipios3.loc[:, ~municipios3.columns.isin(['geometry', 'CÓD. MUN.'])])
 
-# Eliminar filas con valores nulos en la columna 'RENDIMIENTO_TONELADAS_HA'
-df.dropna(subset=['RENDIMIENTO_TONELADAS_HA'], inplace=True)
+# Aplicar K-Means
+kmeans = KMeans(n_clusters=10)
+np.random.seed(1234)
+k10cls = kmeans.fit(db_scaled)
+k10cls.labels_[:5]
+
+# Agregar las etiquetas al DataFrame de municipios
+municipios3["k10cls"] = k10cls.labels_
+
+# Fusionar DataFrame de evaluaciones y municipios3
+evaluaciones3 = pd.merge(evaluaciones, municipios3[['CÓD. MUN.', 'k10cls']], on='CÓD. MUN.', how='left')
+
+# Filtrar y limpiar datos
+evaluaciones3 = evaluaciones3.dropna(subset='Rendimiento\n(t/ha)')
+evaluaciones3 = evaluaciones3.dropna(subset='AÑO')
+evaluaciones3 = evaluaciones3.dropna(subset='CULTIVO')
+
+# Seleccionar columnas de interés
+columnas = ['GRUPO \nDE CULTIVO', 'AÑO', 'MUNICIPIO', 'Rendimiento\n(t/ha)']
+df = evaluaciones3[columnas]
+
+# Eliminar filas con valores nulos en la columna 'Rendimiento\n(t/ha)'
+df.dropna(subset=['Rendimiento\n(t/ha)'], inplace=True)
 
 # Codificar variables categóricas
 le = LabelEncoder()
-df['NOMBRE_CULTIVO'] = le.fit_transform(df['NOMBRE_CULTIVO'])
-df['NOMBRE_MUNICIPIO'] = le.fit_transform(df['NOMBRE_MUNICIPIO'])
+df['GRUPO \nDE CULTIVO'] = le.fit_transform(df['GRUPO \nDE CULTIVO'])
+df['MUNICIPIO'] = le.fit_transform(df['MUNICIPIO'])
 
 # Definir variables X y Y
-X = ['NOMBRE_CULTIVO', 'ANIO', 'NOMBRE_MUNICIPIO']
-Y = 'RENDIMIENTO_TONELADAS_HA'
+X = ['GRUPO \nDE CULTIVO', 'AÑO', 'MUNICIPIO']
+Y = 'Rendimiento\n(t/ha)'
 X = df[X]
 Y = df[Y]
 
 # Dividir datos en conjuntos de entrenamiento y prueba
-X_train, X_test, y_train, y_test = train_test_split(X, Y)
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.25, random_state = 0)
 
-# Crear características polinómicas
-degree = 2
-poly = PolynomialFeatures(degree=degree)
-X_train_poly = poly.fit_transform(X_train)
-X_test_poly = poly.fit_transform(X_test)
 
-# Crear y entrenar el modelo
-modelo = LinearRegression()
-modelo.fit(X_train_poly, y_train)
+# Registrar el experimento
+experiment = mlflow.set_experiment("Polynomial")
+# Nombre incremental
+time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-# Predecir con el conjunto de prueba
-y_pred = modelo.predict(X_test_poly)
+run_name = f"Polynomial-{time}"
 
-# Calcular métricas de evaluación
-mse = mean_squared_error(y_test, y_pred)
-mae = mean_absolute_error(y_test, y_pred)
+with mlflow.start_run(run_name=run_name):
+    # Define Parametros
+    # Crear características polinómicas
+    degree = 2    
+    poly = PolynomialFeatures(degree=degree)
+    X_train_poly = poly.fit_transform(X_train)
+    X_test_poly = poly.fit_transform(X_test)
 
-# Imprimir resultados
-print(f'MSE: {mse}')
-print(f'MAE: {mae}')
+    # Crear y entrenar el modelo de regresión lineal
+    modelo = LinearRegression()
+    modelo.fit(X_train_poly, y_train)
+
+    # Predecir con el conjunto de prueba
+    y_pred = modelo.predict(X_test_poly)
+
+    # Registrar los Parámetros
+    mlflow.log_param("Degree_Polynomial", degree)
+    mlflow.log_param("clusters", 10)
+
+    # Registrar el Modélo
+    mlflow.sklearn.log_model(modelo, "Polynomial")
+
+    # Calcular métricas de evaluación
+    mse = mean_squared_error(y_test, y_pred)
+    mae = mean_absolute_error(y_test, y_pred)
+    mlflow.log_metric("mse", mse)
+    mlflow.log_metric("mae", mae)
+    
+    # Imprimir resultados
+    print(f'MSE: {mse}')
+    print(f'MAE: {mae}')
+
+    # Finaliza el tracking de experimentos con MLflow
+    mlflow.end_run()
