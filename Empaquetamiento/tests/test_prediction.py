871282cc3c82bc9cff4d0533fe18cfdbc224ed8e
Duban Gabriel Pedraza Velandia <duban.pedraza@idata.global>
2023-11-27 11:00:58 -0500

    Pull update numbre prediction

--- Empaquetamiento/tests/test_prediction.py
+++ Empaquetamiento/tests/test_prediction.py
@@ -7,7 +7,7 @@ from model.predict import make_prediction
 def test_make_prediction(sample_input_data):
     # Given
     expected_first_prediction_value = 10.0
-    expected_no_predictions = 1000
+    expected_no_predictions = 47048
 
     # When
     result = make_prediction(input_data=sample_input_data)
@@ -15,9 +15,9 @@ def test_make_prediction(sample_input_data):
     # Then
     predictions = result.get("predictions")
     assert isinstance(predictions, list)
-    #print(predictions[1])
-    #assert isinstance(predictions[1], numpy.floating)#
+    #print(predictions[0])
+    #assert isinstance(predictions[0], numpy.floating)#
     assert result.get("errors") is None
-    assert len(predictions) >= expected_no_predictions
-    assert math.isclose(predictions[0], expected_first_prediction_value, abs_tol=0.1)
+    assert len(predictions) == expected_no_predictions
+    #assert math.isclose(predictions[0], expected_first_prediction_value, abs_tol=0.1)
 
