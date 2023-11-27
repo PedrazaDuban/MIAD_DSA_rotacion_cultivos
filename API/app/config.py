c1f258945642e4cba53a32cf9394b44e73ca5b15
Duban Gabriel Pedraza Velandia <duban.pedraza@idata.global>
2023-11-27 13:07:37 -0500

    upadate import

--- API/app/config.py
+++ API/app/config.py
@@ -4,7 +4,8 @@ from types import FrameType
 from typing import List, cast
 
 from loguru import logger
-from pydantic import AnyHttpUrl, BaseSettings
+#from pydantic import AnyHttpUrl, BaseSettings
+from pydantic_settings import AnyHttpUrl, BaseSettings
 
 # Nivel del logger
 class LoggingSettings(BaseSettings):
