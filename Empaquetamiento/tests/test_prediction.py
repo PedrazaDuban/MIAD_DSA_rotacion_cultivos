f532e1c8024abf02ee889592bbea272964df7a10
Duban Gabriel Pedraza Velandia <duban.pedraza@idata.global>
2023-11-27 10:34:42 -0500

    parametros de test

--- Empaquetamiento/tests/test_prediction.py
+++ Empaquetamiento/tests/test_prediction.py
@@ -7,7 +7,7 @@ from model.predict import make_prediction
 def test_make_prediction(sample_input_data):
     # Given
     expected_first_prediction_value = 10.0
-    #expected_no_predictions = 47048
+    expected_no_predictions = 1000
 
     # When
     result = make_prediction(input_data=sample_input_data)
@@ -18,6 +18,6 @@ def test_make_prediction(sample_input_data):
     print(predictions[0])
     #assert isinstance(predictions[0], numpy.floating)
     assert result.get("errors") is None
-    #assert len(predictions) == expected_no_predictions
+    assert len(predictions) >= expected_no_predictions
     assert math.isclose(predictions[0], expected_first_prediction_value, abs_tol=0.01)
 
